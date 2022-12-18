d5fe8fd526cb42a1ac0536b808a9bf7eed1c1d60
Nico Madariaga <nico_madariaga@gmail.com>
2022-12-18 10:07:51 -0300

    Module datatime

--- Modularidad/datetime_module.py
+++ Modularidad/datetime_module.py
@@ -13,9 +13,39 @@ print(dt.minute)       # minutos
 print(dt.second)       # segundos
 print(dt.microsecond)  # microsegundos
 
-print("{}:{}:{}".format(dt.hour, dt.minute, dt.second))
-print(dt.strftime("%d/%m/%Y"))
-print("{}/{}/{}".format(dt.day, dt.month, dt.year))
-print(dt.isoformat())
+print("{}:{}:{}".format(dt.hour, dt.minute, dt.second)) #hour:minute:second
+print(dt.strftime("%d/%m/%Y"))#day/month/year
+print("{}/{}/{}".format(dt.day, dt.month, dt.year)) #day/month/year
+print(dt.isoformat()) #2022-12-17T11:07:54.945615
 
-print(type(dt))
+print(type(dt)) #<class 'datetime.datetime'>
+
+print(dt.timestamp()) #segundos transcurridos desde el primero de enero de 1970 a las 0 horas UTC hasta el momento a representar.
+
+
+###Utilizamos el modulo time###
+from datetime import time
+
+current_tame = time( 12, 25, 0)
+
+print(current_tame.hour)
+print(current_tame.minute)
+print(current_tame.second)
+
+
+###Utilizamos el modulo date###
+from datetime import date
+
+current_date = date(2022, 12, 17)
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date.today()#La fecha de hoy
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+#Operaciones
+current_date = date(current_date.year, current_date.month, current_date.day+1)
+print(current_date.year, current_date.month, current_date.day)
